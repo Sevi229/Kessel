bf96a54122eec507e1dcd225802b379c11184b79
luga3 <161241300+luga3@users.noreply.github.com>
2024-03-27 21:28:12 +0100

    wip

---
+++ main/Test.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+    // Ihr Code, der ausgeführt wird, sobald der DOM vollständig geladen ist.
+    document.getElementById('myDiv').textContent = 'Text, der sofort nach dem Laden des DOM hinzugefügt wird.';
+});
