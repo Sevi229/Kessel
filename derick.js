ab835cc6f10c0b31dedbf405c4863e99be0725c0
Sevi229 <severin.iosgg@gmail.com>
2024-04-04 10:55:11 +0200

    Show NEXT NUMBERS

---
+++ derick.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// Define the path to the main directory
+const mainDir = path.join(__dirname, 'main');
+
+// Serve the index.html file from the main directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(mainDir, 'derick.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(mainDir, 'Roule'));
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
