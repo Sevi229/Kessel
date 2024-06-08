9689dee1b80db5d23a9cbfbcf03eb35d5a1981c4
Sevi229 <139388082+Sevi229@users.noreply.github.com>
2024-06-08 19:12:59 +0200

    2.5
    2.5

--- derick.js
+++ derick.js
@@ -9,7 +9,7 @@ const mainDir = path.join(__dirname, 'main');
 
 // Serve the index.html file from the main directory
 app.get('/', (req, res) => {
-  res.sendFile(path.join(mainDir, 'V2.4.html'));
+  res.sendFile(path.join(mainDir, 'V2.5.html'));
 });
 
 app.get('/', (req, res) => {
