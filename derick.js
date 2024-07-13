4f04c257991e33f8d5eeb07a717d5699b5073a31
Sevi229 <139388082+Sevi229@users.noreply.github.com>
2024-07-13 19:41:06 +0200

    asdasd
    Vsd

--- derick.js
+++ derick.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 // Define the path to the main directory
 const mainDir = path.join(__dirname, 'main');
 
 // Serve the index.html file from the main directory
 app.get('/', (req, res) => {
-  res.sendFile(path.join(mainDir, 'V3.5W.html'));
+  res.sendFile(path.join(mainDir, 'V3.8W.html'));
 });
 
 app.get('/', (req, res) => {
